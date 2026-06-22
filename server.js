0c6722fd963aca512df9cfca7577c16fd222b7d5
Swayam-The Coder <swayamcode1@gmail.com>
2026-06-22 12:48:52 +0530

    MongoDB Connection successful

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+connectDB();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
