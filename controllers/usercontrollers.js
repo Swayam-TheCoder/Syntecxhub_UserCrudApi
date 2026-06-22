775e70947ec7266e6390e15ce580f84fbe10870f
Swayam-The Coder <swayamcode1@gmail.com>
2026-06-22 14:08:10 +0530

    get all Users, get single user by id and update functionality

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -16,5 +16,43 @@ exports.createUser = async (req, res) => {
   } catch(error){
     res.status(500).json({message: error.message});
   }
+};
+
+// get users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await user.find();
+    res.status(200).json(users);
+  } catch(error) {
+    res.status(500).json({message: error.message});
+  }
+};
+
+// get user by id / single user
+
+exports.getUserById = async (req, res) => {
+  try {
+    const users = await user.findById(req.params.id);
+    if(!users){
+      return res.status(404).json({message: 'User not found'});
+    }
+    res.status(200).json(users);
+  } catch(error){
+    res.status(500).json({message: error.message});
+  }
 }
 
+// update user by id
+
+exports.updateUser = async (req, res) => {
+  try {
+    const updateduser = await user.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if(!updateduser){
+      return res.status(404).json({message: 'User not found'});
+    }
+
+    res.status(200).json({message: 'User updated successfully', user: updateduser});
+  } catch(error){
+    res.status(500).json({message: error.message});
+  }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { createUser } = require('../controllers/usercontrollers');
+const { createUser, getUsers, getUserById, updateUser } = require('../controllers/usercontrollers');
 
-// Route to create a new user
+// create a new user
 router.post('/', createUser);
+
+// get all users
+router.get('/', getUsers);
+
+// get a single user by ID
+router.get('/:id', getUserById);
+
+// update a user by ID
+router.put('/:id', updateUser);
 
 module.exports = router;  
